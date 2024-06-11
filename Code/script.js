8e74b0b68f51fcb6cb989ac802ddb50cd5e77f9f
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-06-11 14:56:25 +0530

    added all files

---
+++ Code/script.js
@@ -0,0 +1,31 @@
+// // INFINITE SCROLLING CAROUSEL ANIMATION
+const scrollers = document.querySelectorAll(".carousel");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+    const scrollerInner = scroller.querySelector(".carousel_inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+// CUSTOM CURSOR
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  console.log(e);
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
