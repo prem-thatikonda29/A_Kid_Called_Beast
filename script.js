fa73390028084d87e0e179ce07749be467e788a4
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-06-12 00:37:09 +0530

    added icon

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function addAnimation() {
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", (e) => {
-  console.log(e);
+  // console.log(e);
   cursor.setAttribute(
     "style",
     "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
